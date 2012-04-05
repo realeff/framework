bb812de8f24e1f6729f26bc17374fd756d8abb82
yibo liang <liangyibo12@gmail.com>
2012-04-06 01:14:52 +0800

    Signed-off-by: yibo liang <liangyibo12@gmail.com>

--- config.php
+++ config.php
@@ -68,48 +68,50 @@ $auth_key = 'd7xphQoX7B8GN1S5JDPKqHITWUOrpiw06E96kgfxaug';
 
 $cookie_domain = '.realeff.com';
 
-// 缓存
-$config['caches'] = array(
-  'default' => array(
+// 会话处理程序
+$config['session_handler'] = 'database';
+
+// 缓存处理程序
+$config['cache_handler'] = array(
+  'cache' => array(
     'driver' => 'file',
     'path'   => '{workspace}/cache',
     'secret'   => 'UOrpiw06E',
-    'lifetime' => 0,
-    'bin' => array(),
+    'lifetime' => 0
    ),
-  'database' => array(
+  'cache' => array(
     'driver' => 'database',
     'querier' => 'cache', // 查询器名称
-    'lifetime' => 0,
-    'split'  => 1,
-    'bin' => array('cache' => 10, 'cache_menu', 'cache_content' => 100) // 容器名称 => 容器数量
+    'lifetime' => 0
    ),
-  'memcache' => array(
-    'driver' => 'memcache',
-    'prefix' => 'rpiw06E96',
-    'lifetime' => 0,
-    'bin' => array(),
-    'option' => array('compress' => 1024, 'saving' => 0.2, 'persistent' => TRUE, 'timeout' => 1, 'retry_interval' => 15, 'status' => TRUE),
-    'server' => array(
-        'host' => '127.0.0.1',
-        'port' => '11211',
-        'persistent' => TRUE,
-        'weight' => 0,
-        'timeout' => 1,
-        'retry_interval' => 15,
-        'status' => TRUE
-     )
-   ),
-  'memcached' => array(
-    'driver' => 'memcached',
-    'prefix' => 'rpiw06E96',
-    'lifetime' => 0,
-    'bin' => array(),
-    'option' => array(),
-    'server' => array(
-        array('host' => '127.0.0.1', 'port' => '11211', 'weight' => 0)
-     )
-   )
+//   'cache' => array(
+//     'driver' => 'memcache',
+//     'lifetime' => 0,
+//     'option' => array(),
+//     'server' => array(
+//         'host' => '127.0.0.1',
+//         'port' => '11211',
+//         'weight' => 0,
+//      )
+//    ),
+//   'cache_session' => array(
+//     'driver' => 'memcache',
+//     'lifetime' => 0,
+//     'option' => array(),
+//     'server' => array(
+//         'host' => '127.0.0.1',
+//         'port' => '11211',
+//         'weight' => 0,
+//      ),
+//    ),
+//   'memcached' => array(
+//     'driver' => 'memcached',
+//     'lifetime' => 0,
+//     'option' => array(),
+//     'server' => array(
+//         array('host' => '127.0.0.1', 'port' => '11211', 'weight' => 0)
+//      )
+//    )
 );
 
 // 站点名称
@@ -119,6 +121,66 @@ $config['site_name'] = 'RealEff';
 // 系统临时文件目录
 //$config['tmp_file_path'] = '';
 
+// 锁定范围IP地址列表
+$config['blocked_ips'] = array('192.168.1.1/24', '10.0.10.0/24', '202.101.25.*');
+// 检查DDOS攻击
+$config['ddos_check'] = TRUE;
+// 禁止机器人访问
+$config['robot_disable'] = TRUE;
+// 机器人列表
+$config['robots'] = array(
+    /* The most common ones. */
+    'Googlebot',
+    'msnbot',
+    'Slurp',
+    'Yahoo',
+    /* The rest alphabetically. */
+    'Arachnoidea',
+    'ArchitextSpider',
+    'Ask Jeeves',
+    'B-l-i-t-z-Bot',
+    'Baiduspider',
+    'BecomeBot',
+    'cfetch',
+    'ConveraCrawler',
+    'ExtractorPro',
+    'FAST-WebCrawler',
+    'FDSE robot',
+    'fido',
+    'geckobot',
+    'Gigabot',
+    'Girafabot',
+    'grub-client',
+    'Gulliver',
+    'HTTrack',
+    'ia_archiver',
+    'InfoSeek',
+    'kinjabot',
+    'KIT-Fireball',
+    'larbin',
+    'LEIA',
+    'lmspider',
+    'Lycos_Spider',
+    'Mediapartners-Google',
+    'MuscatFerret',
+    'NaverBot',
+    'OmniExplorer_Bot',
+    'polybot',
+    'Pompos',
+    'Scooter',
+    'Teoma',
+    'TheSuBot',
+    'TurnitinBot',
+    'Ultraseek',
+    'ViolaBot',
+    'webbandit',
+    'www.almaden.ibm.com/cs/crawler',
+    'ZyBorg'
+);
+// 自定义加载安全检查文件
+$config['custom_firewall_php'] = '';
+// 支持移动设备访问
+$config['mobile_enable'] = TRUE;
 
 // 支持移动平台
 // 机器人访问
